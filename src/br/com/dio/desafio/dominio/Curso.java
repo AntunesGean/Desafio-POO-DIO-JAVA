9654125ad587e9bcb9e10626469e5042a1a8bd90
AntunesGean <gean.antunes@gmail.com>
2023-04-06 15:52:42 -0300

    criação do projeto para a mentoria de POO

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,51 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+    private String nome;
+    private String descricao;
+    private int cargaHoraria;
+
+    // Construtor padrão
+    public Curso() {
+    }
+
+    // Construtor com parâmetros
+    public Curso(String nome, String descricao, int cargaHoraria) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso: {" +
+                "nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
